ee4919b1afd45a33bc37250dbfae18cf789d5bf6
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-06-18 11:45:59 -0300

    Criação do modal

--- src/Components/About/AboutMe.jsx
+++ src/Components/About/AboutMe.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Modal from '../Modal/Modal';
 import './aboutMe.css';
 import {FaAward} from 'react-icons/fa'
 import {AiFillFolderOpen} from 'react-icons/ai'
 import {HiAcademicCap} from 'react-icons/hi'
 
 const AboutMe = () => {
+  const modalExperience = useRef()
+  const modalFormation = useRef()
+  const modalProject = useRef()
   return (
     <section className="about">
       <h1>Sobre Mim</h1>
@@ -22,18 +26,53 @@ const AboutMe = () => {
             <article className="about_card">
               <FaAward className="about_icon" />
               <h5>Experiência</h5>
+              <button onClick={() => modalExperience.current.open()} className="btn-cards">Ver</button>
             </article>
 
             <article className="about_card">
               <HiAcademicCap className="about_icon" />
               <h5>Formação</h5>
+              <button onClick={() => modalFormation.current.open()} className="btn-cards">Ver</button>
             </article>
 
             <article className="about_card">
               <AiFillFolderOpen className="about_icon" />
               <h5>Projetos</h5>
+              <button onClick={() => modalProject.current.open()} className="btn-cards">Ver</button>
             </article>
           </div>
+          <Modal ref={modalExperience}>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={() => modalExperience.current.close()}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <p>Tenho experiência na area financeira.</p>
+          </Modal>
+
+          <Modal ref={modalFormation}>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={() => modalFormation.current.close()}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <p>Sou formado em Ciências Contábeis.</p>
+          </Modal>
+
+          <Modal ref={modalProject}>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={() => modalProject.current.close()}npm i gh-pages
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <p>Esses são alguns projetos que eu mais me orgulho.</p>
+          </Modal>
         </div>
         
       </div>             
